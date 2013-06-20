c0b22b9573e3b456b2254445adabb1768be4ec19
linyows <linyows@gmail.com>
2013-06-20 17:31:59 +0900

    bump version to 1.2.1

--- github-wiki-search.user.js
+++ github-wiki-search.user.js
@@ -5,7 +5,7 @@
 // @include        https://github.com/*/*/wiki*
 // @namespace      http://userscripts.org/scripts/show/129930
 // @author         linyows <linyows@gmail.com>
-// @version        1.2.0
+// @version        1.2.1
 // ==/UserScript==
 
 (function(){
